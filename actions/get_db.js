36b945725016e4961718282f954b025c553b7c4a
Glynn Bird <glynn.bird@gmail.com>
2017-05-24 16:55:32 +0100

    allow GET /db/docid to work

--- actions/get_db.js
+++ actions/get_db.js
@@ -1,5 +1,7 @@
 var security = require('./lib/security.js');
 var cloudant = require('./lib/db.js');
+var access = require('./lib/access.js');
+var utils = require('./lib/utils.js');
 
 function main(msg) {
 
@@ -8,7 +10,23 @@ function main(msg) {
   // cloudant
   var db = cloudant.configure(msg.COUCH_HOST, msg.ENVOY_DATABASE_NAME);
 
-  return db.info();
+  // if we have been passed a path, then this is a GET /db/<docid> request
+  if (msg.__ow_path && msg.__ow_path.length) {
+    var params = utils.allowParams(['attachments', 'conflicts', 'local_seq', 'open_revs', 'rev', 'revs', 'revs_info'], msg);
+    var bits = msg.__ow_path.split('/');
+    var id = decodeURIComponent(bits[1]);
+    var envoyid = access.addOwnerId(id, user_id)
+    return db.get(envoyid, params).then(function(data) {
+      var retval = {};
+      retval[id] = access.strip(data);
+      return retval;
+    });
+  } else {
+    // it's a GET /db/ request
+    return db.info();
+  }
+
+  
 };
 
 exports.main = main;
